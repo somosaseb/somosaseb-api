94a3689625ca0197af60a2228771b159c231e222
Mario César <mariocesar.c50@gmail.com>
2021-08-22 09:52:36 -0400

    Adds JSON responses to the admin changelist views

--- src/aseb/apps/users/admin.py
+++ src/aseb/apps/users/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin, get_user_model
 from django.contrib.auth.models import Group
 
+from aseb.core.admin import APIAdminModel
+
 User = get_user_model()
 
 admin.site.unregister(Group)
 
 
 @admin.register(User)
-class UserAdmin(auth_admin.UserAdmin):
+class UserAdmin(auth_admin.UserAdmin, APIAdminModel):
     search_fields = ["email", "first_name", "last_name"]
     fieldsets = (
         (None, {"fields": ("email", "password", "username")}),
@@ -25,9 +27,10 @@ class UserAdmin(auth_admin.UserAdmin):
         ("Important dates", {"fields": ("last_login", "date_joined")}),
         (None, {"fields": ("secret_key",)}),
     )
-
     list_display = (
-        "__str__",
+        "email",
+        "first_name",
+        "last_name",
         "is_active",
         "is_staff",
         "is_superuser",
--- src/aseb/core/admin.py
+++ src/aseb/core/admin.py
@@ -1,5 +1,14 @@
+from pprint import pprint
+
 from django.contrib import admin
+
+from django.contrib.admin.utils import lookup_field
+from django.contrib.admin.views import main as admin_main_views
+from django.contrib.admin.views.main import ChangeList
+from django.core.exceptions import ObjectDoesNotExist
 from django.forms import forms
+from django.http import HttpRequest, JsonResponse
+from rest_framework.utils.mediatypes import media_type_matches
 
 
 class AdminSite(admin.AdminSite):
@@ -28,3 +37,62 @@ class AdminAuditedModel(admin.ModelAdmin):
             obj.created_by = request.user
 
         super().save_model(request, obj, form, change)
+
+
+# Monkey way to allow a new parameter to the changelist view ¯\_(ツ)_/¯
+admin_main_views.IGNORED_PARAMS = [
+    "format",
+    *admin_main_views.IGNORED_PARAMS,
+]
+
+
+def is_api_list(request: HttpRequest):
+    if request.method != "GET":
+        return False
+
+    return request.GET.get("format", "") == "json" or media_type_matches(
+        "application/json",
+        request.content_type,
+    )
+
+
+def serialize_items(
+    cl: ChangeList,
+):
+    def iterator(result):
+        for field_name in cl.list_display:
+            if field_name == "action_checkbox":
+                continue
+
+            try:
+                f, attr, value = lookup_field(field_name, result, cl.model_admin)
+            except ObjectDoesNotExist:
+                yield field_name, None
+            else:
+                if f is None or f.auto_created:
+                    result_repr = repr(value)
+                else:
+                    result_repr = value
+
+                yield field_name, result_repr
+
+    return [{name: value for name, value in iterator(res)} for res in cl.result_list]
+
+
+class APIAdminModel(admin.ModelAdmin):
+    def changelist_view(self, request: HttpRequest, extra_context=None):
+        response = super().changelist_view(request, extra_context)
+
+        if is_api_list(request):
+            changelist = response.context_data["cl"]
+
+            return JsonResponse(
+                {
+                    "results": serialize_items(changelist),
+                    "page": changelist.page_num,
+                    "per_page": changelist.paginator.per_page,
+                    "count": changelist.paginator.count,
+                }
+            )
+
+        return response
