e543465e17c3d2ada7618cc61817596a1c3e0342
Mario César <mariocesar.c50@gmail.com>
2021-08-23 20:47:46 -0400

    Custom authentication for api

---
+++ src/aseb/api/authentication.py
@@ -0,0 +1,22 @@
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt import authentication
+from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.settings import api_settings
+
+
+class JWTAuthentication(authentication.JWTAuthentication):
+    def get_user(self, validated_token):
+        try:
+            user_id = validated_token[api_settings.USER_ID_CLAIM]
+        except KeyError:
+            raise InvalidToken(_("Token contained no recognizable user identification"))
+
+        try:
+            user = self.user_model.objects.get(**{api_settings.USER_ID_FIELD: user_id})
+        except self.user_model.DoesNotExist:
+            raise AuthenticationFailed(_("User not found"), code="user_not_found")
+
+        if not user.is_active:
+            raise AuthenticationFailed(_("User is inactive"), code="user_inactive")
+
+        return user
