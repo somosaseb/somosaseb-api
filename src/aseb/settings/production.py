be0e3b90fbff75a5da20fc03ee6466e2991333ed
Mario César <mariocesar.c50@gmail.com>
2021-08-28 00:22:41 -0400

    Error tracking

--- src/aseb/settings/production.py
+++ src/aseb/settings/production.py
@@ -1,3 +1,6 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.logging import LoggingIntegration
 from .base import *  # noqa
 
 DEBUG = False
@@ -5,3 +8,13 @@ DEBUG = False
 ALLOWED_HOSTS = env("ALLOWED_HOSTS", "*")  # noqa:
 
 DATABASES["default"]["CONN_MAX_AGE"] = env("CONN_MAX_AGE", 60)  # noqa:
+
+sentry_sdk.init(
+    dsn=env("SENTRY_DSN"),
+    integrations=[
+        DjangoIntegration(),
+        LoggingIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True,
+)
