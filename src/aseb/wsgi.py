6ede6a8c54df32cfa427da28701b0c9304572036
Mario César <mariocesar.c50@gmail.com>
2021-09-06 01:59:09 -0400

    Stamp version on all responses

--- src/aseb/wsgi.py
+++ src/aseb/wsgi.py
@@ -1,7 +1,35 @@
 import os
+import django
+from datetime import datetime
+from django.core.handlers.wsgi import WSGIHandler
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aseb.settings.develop")
+
+
+class WSGIApplication(WSGIHandler):
+    def __init__(self, *args, **kwargs):
+        django.setup(set_prefix=False)
+        os.environ.setdefault("DEPLOY_VERSION", "develop")
+        os.environ.setdefault("DEPLOY_DATETIME", datetime.utcnow().isoformat())
+
+        deploy_version = os.environ.get("DEPLOY_VERSION")
+        deploy_datetime = os.environ.get("DEPLOY_DATETIME")
+
+        self.served_by = "rev={};date={}".format(deploy_version, deploy_datetime)
+
+        super().__init__(*args, **kwargs)
+
+    def get_response(self, request):
+        response = super().get_response(request)
+        response["X-Served-By"] = self.served_by
+
+        return response
+
+
+def get_wsgi_application():
+    django.setup(set_prefix=False)
+    return WSGIApplication()
+
 
 application = get_wsgi_application()
